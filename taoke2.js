3ba6c9d53b82cd42ab39c10c954ff96eb193e0e1
wangqingxue <313071500@qq.com>
2018-08-14 14:58:56 +0800

    最终版本

--- taoke2.js
+++ taoke2.js
@@ -11,82 +11,98 @@
 
 
 function request(arr) {
-    let errorary = [];
+    let errorary = []
     console.log('我应该请求',arr.length,'次')
     arr.forEach((item)=>{
-        let url = `http://www.dataoke.com/item?id=${item.id}`;
+        let url = `http://www.dataoke.com/item?id=${item.id}`
         let flag = localStorage.getItem(`id_numid_${item.id}`)
         // 找到则不需要再次请求numid
         if(flag){
-            console.log('无需请求numid，已经存在numid');
+            item.numid = flag
+            console.log('无需请求numid，已经存在numid')
         }else{
             $.ajax({
                 url: url,
                 async: false,
                 success: function (responseText) {
                     try {
-                        item.numid = responseText.match(/item\.htm\?id=([0-9]+)/)[1];
+                        item.numid = responseText.match(/item\.htm\?id=([0-9]+)/)[1]
                         localStorage.setItem(`id_numid_${item.id}`,item.numid)
                     } catch (error) {
-                        errorary.push(item);
+                        errorary.push(item)
                     }
                 }
             })
         }
     })
     if(errorary.length==0){
-        console.log('请求结束');
+        console.log('请求结束')
     }else{
-        request(errorary);
+        request(errorary)
     }
 }
 
 // 发送不需要修改
-function ajax(data){
+function saveGoodsList(data){
+    let temp = {data:data,msg:'saveGoodsList'}
+    temp = JSON.stringify(temp)
     $.ajax({
         url:'http://admin.api.huaqiweb.com/mock/51/tbk/assistant/tbkRushItem',
         type:'post',
         contentType:'application/json',
         async: false,
-        data:data,
+        data:temp,
         success:function (responseText) {
             console.log('OK')
         },
         error:function(err){
-            console.log(err,'发生错误');
+            console.log(err,'发生错误')
         }
     })
 }
-
+function clear(){
+    let oldDate = localStorage.getItem('oldDate')
+    let currentDay = new Date().toLocaleDateString()
+    if(new Date().toLocaleDateString() === oldDate){
+        oldDate = currentDay
+        localStorage.setItem('oldDate',oldDate)
+        console.log("not clear")
+    }else{
+        console.log("clear localStorage")
+        localStorage.clear()
+        localStorage.setItem('oldDate',currentDay)
+    }
+   
+}
 function init() {
-    'use strict';
-    let goodsList = dataDef;
-    let data = [];
+    'use strict'
+    let goodsList = dataDef
+    let data = []
     for(key in goodsList){
         goodsList[key].forEach((item) => {
             let flag = localStorage.getItem(`id_md5_${item.id}`)
-            console.log(flag)
             if(flag!=md5(JSON.stringify(item))){
-                console.log('需存储md5')
                 data.push(item);
                 localStorage.setItem(`id_md5_${item.id}`,md5(JSON.stringify(item)))
             }
         });
     }
-    console.log(data,'data');
-    console.log(data.length,'data.length');
+    console.log(data.length,'data.length')
     if(data.length>0){
         console.log('需要请求numid',data.length,'次')
         request(data);
-        let temp = {data:data,msg:'et sed minim'};
-        temp = JSON.stringify(temp);
-        ajax(temp);
+        let times = Math.ceil(data.length/100)
+        for(let i=0;i<times;i++){
+            saveGoodsList(data.splice(0,100))
+        }
     }else{
-        console.log('数据没有发生变化，无需请求numid')
+        console.log('商品数据没有发生变化')
     }
 };
+clear();
 init();
+
 // 半小时刷新当前页面，再次执行代码
 setTimeout(function(){
-    history.go(0);
-},1800*1000)
+    history.go(0)
+},900000)
